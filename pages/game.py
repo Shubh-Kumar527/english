48324a954f60d0d906addd231031ac08c79b2a91
Shubh-Kumar527 <shubhstudios527@gmail.com>
2025-07-29 15:25:23 +0530

    Update game.py

--- pages/game.py
+++ pages/game.py
@@ -75,5 +75,5 @@ with g1:
 with g2:
     st.image(image2, caption='My Image', use_container_width=True)
     if st.button("game2"):
-        st.switch_page("pages/students_creation_story.py")
+        st.switch_page("pages/guess_the_authers_game.py")
 
