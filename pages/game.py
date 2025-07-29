e2655f28d1e61fa616804c474e481862b2762c3d
Shubh-Kumar527 <shubhstudios527@gmail.com>
2025-07-29 15:23:57 +0530

    Update game.py

--- pages/game.py
+++ pages/game.py
@@ -61,9 +61,9 @@ elif selected_path == "pages/about_us.py":
 g1, g2 = st.columns(2)
 
 # Load the image
-image = Image.open('images/creativity.jpg')
+image1 = Image.open('images/creativity.jpg')
 
-image1 = Image.open('images/poems.jpg')
+image2 = Image.open('images/poems.jpg')
 
 
 
