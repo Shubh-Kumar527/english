2ea9cc7b2fcf4a2b851385ec16587d3c5c8e8adc
Shubh-Kumar527 <shubhstudios527@gmail.com>
2025-07-30 15:19:46 +0530

    Update students_creation.py

--- pages/students_creation.py
+++ pages/students_creation.py
@@ -70,13 +70,13 @@ image2 = Image.open('images/story.jpg')
 
 with poem:
     st.image(image1, caption='My Image', use_container_width=True)
-    if st.button("Self Composed Poem"):
+    if st.button("Poetry Palette"):
         st.switch_page("pages/students_creation_poem.py")
 with story:
     st.image(image2, caption='My Image', use_container_width=True)
-    if st.button("Self Written Stories"):
+    if st.button("Tales & Treasure"):
         st.switch_page("pages/students_creation_story.py")
 with creativity:
     st.image(image, caption='My Image', use_container_width=True)
-    if st.button("Creative Minds"):
+    if st.button("Let's Get Writing"):
         st.switch_page("pages/students_creation_creativity.py")
