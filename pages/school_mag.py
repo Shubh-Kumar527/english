3709d16cfc9074039df0215413724633b3e420bf
Shubh-Kumar527 <shubhstudios527@gmail.com>
2025-08-02 10:11:07 +0530

    Update school_mag.py

--- pages/school_mag.py
+++ pages/school_mag.py
@@ -14,6 +14,45 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Navigation bar
+# # Navigation menu dictionary
+menu = {
+    "Home": "Home.py",
+    "Author Avenue": "pages/About_the_authers.py",
+    "Bright Minds, Bold Words": "pages/students_creation.py",
+    "Brain Battles": "pages/game.py",
+    "School magazine" : "pages/school_mag.py",
+    "About Us": "pages/about_us.py"
+}
+
+# Create columns for nav buttons
+cols = st.columns(len(menu))
+
+# Track selected path
+selected_path = None
+
+# Render buttons and capture selected path
+for i, (label, path) in enumerate(menu.items()):
+    if cols[i].button(label):
+        selected_path = path
+
+# Divider
+st.markdown("---")
+
+# Content logic based on selected path
+if selected_path == "Home.py":
+    st.switch_page("Home.py")
+elif selected_path == "pages/students_creation.py":
+        st.switch_page("pages/students_creation.py")
+elif selected_path == "pages/About_the_authers.py":
+        st.switch_page("pages/About_the_authers.py")
+elif selected_path == "pages/game.py":
+    st.switch_page("pages/game.py")
+elif selected_path == "pages/about_us.py":
+    st.switch_page("pages/about_us.py")
+
+
+
 st.markdown("<br>", unsafe_allow_html=True)
 
 # 📌 Change this to set the width of the image (in pixels)
